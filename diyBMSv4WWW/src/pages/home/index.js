19ade04c16b3dadcd7cdf9630fb631925412ce3e
remyar <remy.arnaudin@wanadoo.fr>
2022-05-12 17:48:10 +0200

    add

--- diyBMSv4WWW/src/pages/home/index.js
+++ diyBMSv4WWW/src/pages/home/index.js
@@ -14,26 +14,7 @@ function Home(props) {
     const settings = props.globalState.settings || {};
     const monitor2 = props.globalState.monitor2 || {};
 
-    let infosTab = [];
 
-    if (monitor2.sent && monitor2.sent > 0) {
-        infosTab.push(<div id="sent" className="stat"><span className="x t">Packets sent:</span><span className="x v">{monitor2.sent}</span></div>);
-    }
-    if (monitor2.received && monitor2.received > 0) {
-        infosTab.push(<div id="received" className="stat"><span className="x t">Packets rec'd:</span><span className="x v">{monitor2.received}</span></div>);
-    }
-    if (monitor2.roundtrip && monitor2.roundtrip > 0) {
-        infosTab.push(<div id="roundtrip" className="stat"><span className="x t">Roundtrip (ms):</span><span className="x v">{monitor2.roundtrip}</span></div>);
-    }
-    if (monitor2.uptime && monitor2.uptime > 0) {
-        infosTab.push(<div id="uptime" className="stat"><span className="x t">Uptime:</span><span className="x v">{utils.secondsToHms(monitor2.uptime)}</span></div>);
-    }
-    if (monitor2.can_sent && monitor2.can_sent > 0) {
-        infosTab.push(<div id="cansent" className="stat"><span className="x t">CAN sent:</span><span className="x v">{monitor2.can_sent}</span></div>);
-    }
-    if (monitor2.can_rec && monitor2.can_rec > 0) {
-        infosTab.push(<div id="canrecd" className="stat"><span className="x t">CAN received:</span><span className="x v">{monitor2.can_rec}</span></div>);
-    }
 
     // Need one color for each pack, could make it colourful I suppose :-)
     const colours = [
@@ -71,14 +52,15 @@ function Home(props) {
                 markLineData.push({ name: 'avg', type: 'average', lineStyle: { color: '#ddd', width: 2, type: 'dotted', opacity: 0.3 }, label: { distance: [10, 0], position: 'start', color: "#eeeeee", textBorderColor: '#313131', textBorderWidth: 2 } });
                 markLineData.push({ name: 'min', type: 'min', lineStyle: { color: '#ddd', width: 2, type: 'dotted', opacity: 0.3 }, label: { distance: [10, 0], position: 'start', color: "#eeeeee", textBorderColor: '#313131', textBorderWidth: 2 } });
                 markLineData.push({ name: 'max', type: 'max', lineStyle: { color: '#ddd', width: 2, type: 'dotted', opacity: 0.3 }, label: { distance: [10, 0], position: 'start', color: "#eeeeee", textBorderColor: '#313131', textBorderWidth: 2 } });
-            
+
                 let xAxis = 0;
-                 for (let index = 0; index < (monitor2?.controllers && monitor2?.controllers[i] && monitor2?.controllers[i].banks) ; index++) {
+                for (let index = 0; index < (monitor2?.controllers && monitor2?.controllers[i] && monitor2?.controllers[i].banks); index++) {
                     markLineData.push({ name: "Bank " + index, xAxis: xAxis, lineStyle: { color: colours[index], width: 4, type: 'dashed', opacity: 0.5 }, label: { show: true, distance: [0, 0], formatter: '{b}', color: '#eeeeee', textBorderColor: colours[index], textBorderWidth: 2 } });
                     xAxis += parseInt(monitor2.controllers[i].seriesmodules);
                 }
 
                 if (monitor2 && monitor2?.controllers && monitor2?.controllers[i]) {
+
                     for (let j = 0; j < monitor2.controllers[i].voltages.length; j++) {
                         labels.push(bankNumber + "/" + cellsInBank);
 
@@ -94,10 +76,15 @@ function Home(props) {
                         if (v > maxVoltage) { maxVoltage = v; }
                         if (v < minVoltage) { minVoltage = v; }
 
+                        if ( maxVoltage > 10 ){ 
+                            maxVoltage = 15.0;
+                            minVoltage = 10.0;
+                        }
+
                         if (monitor2.controllers[i].minvoltages) {
                             voltagesmin.push((parseFloat(monitor2.controllers[i].minvoltages[j]) / 1000.0));
                         }
-                        if (monitor2.maxvoltages) {
+                        if (monitor2.controllers[i].maxvoltages) {
                             voltagesmax.push((parseFloat(monitor2.controllers[i].maxvoltages[j]) / 1000.0));
                         }
 
@@ -116,6 +103,46 @@ function Home(props) {
                     }
                 }
                 comp.push(<h2 style={{ paddingBottom: "15px" }}>Controller N° {i + 1}</h2>);
+
+                let _c = monitor2.controllers && monitor2.controllers[i] || [];
+                let infosTab = [];
+                let totalVoltage = 0;
+
+                let _Range = 0.0;
+                let _Max = 0.0;
+                let _Min = 900000;
+                voltages.forEach((_v) => {
+                    totalVoltage+=_v.value;
+
+                    _Max = Math.max(_Max , _v.value);
+                    _Min = Math.min(_Min , _v.value);
+                });
+
+                infosTab.push(<div id={"range"+i} className="stat"><span className="x t">Range:</span><span className="x v">{((_Max - _Min)*1000).toFixed(2) + " mV"}</span></div>);
+                infosTab.push(<div id={"voltage"+i} className="stat"><span className="x t">Voltage:</span><span className="x v">{totalVoltage.toFixed(2) + " V"}</span></div>);
+
+                if (_c.sent && _c.sent > 0) {
+                    infosTab.push(<div id="sent" className="stat"><span className="x t">Packets sent:</span><span className="x v">{_c.sent}</span></div>);
+                }
+                if (_c.received && _c.received > 0) {
+                    infosTab.push(<div id="received" className="stat"><span className="x t">Packets rec'd:</span><span className="x v">{_c.received}</span></div>);
+                }
+                if (_c.roundtrip && _c.roundtrip > 0) {
+                    infosTab.push(<div id="roundtrip" className="stat"><span className="x t">Roundtrip (ms):</span><span className="x v">{_c.roundtrip}</span></div>);
+                }
+                if (_c.uptime && _c.uptime > 0) {
+                    infosTab.push(<div id="uptime" className="stat"><span className="x t">Uptime:</span><span className="x v">{utils.secondsToHms(_c.uptime)}</span></div>);
+                }
+                if (_c.can_sent && _c.can_sent > 0) {
+                    infosTab.push(<div id="cansent" className="stat"><span className="x t">CAN sent:</span><span className="x v">{_c.can_sent}</span></div>);
+                }
+                if (_c.can_rec && _c.can_rec > 0) {
+                    infosTab.push(<div id="canrecd" className="stat"><span className="x t">CAN received:</span><span className="x v">{_c.can_rec}</span></div>);
+                }
+
+                comp.push(<Infos infosTab={infosTab} />);
+                
+
                 comp.push(<div className="graphs">
                     <Graph id={"graph" + i}
                         markLine={markLineData}
@@ -129,13 +156,15 @@ function Home(props) {
                             , { name: 'CellTemperature', data: tempext }]}
                     />
                 </div>)
+
+               
             }
             return comp;
         })()}
 
 
 
-        <Infos infosTab={infosTab} />
+
     </div>;
 }
 
